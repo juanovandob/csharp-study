fe55a391be7f6fe8e64374f0a4db5e1aeeb4dee4
Juan Carlos <juanovandob@outlook.com>
2024-12-30 01:08:46 -0600

    bibliotecas-net init

---
+++ freecodecamp-csharp/src/Bibliotecas-net/Program.cs
@@ -0,0 +1,17 @@
+﻿//Uso de las bibliotecas .NET
+//System.Random and System.Console
+
+//in actual versions of .NET, you can use Random dice = new(); instead of Random dice = new Random();
+//Random dice = new Random();
+Random dice = new();
+int roll = dice.Next(1, 7);
+Console.WriteLine(roll);
+
+//overload versions of the Next method
+int roll1 = dice.Next();        // 0 to 2,147,483,647
+int roll2 = dice.Next(101);     // 0 to 100
+int roll3 = dice.Next(50, 101); // 50 to 100
+
+Console.WriteLine($"First roll: {roll1}");
+Console.WriteLine($"Second roll: {roll2}");
+Console.WriteLine($"Third roll: {roll3}");
